3d605d398616d7ebd0cff29112dcead346b2d12b
MBX619 <123102601+MBX619@users.noreply.github.com>
2023-02-07 22:31:57 +0100

    TASK 9

--- src/main/kotlin/lms_126/Task9.kt
+++ src/main/kotlin/lms_126/Task9.kt
@@ -2,5 +2,19 @@ package lms_126
 
 fun task9(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        1. INPUT CC to machine
+        2. INPUT CC CODE 
+        3. IF CC code is correct:
+             3.INPUT amount to withdraw
+             4.IF amount to withdraw > user`s balance
+                 5. OUTPUT request new amount
+             6.ELSE:
+                 7.output money to user 
+                 8.Update user new balance
+                 9.OUTPUT payslip
+                 10.OUTPUT card
+        4.ELSE 
+             1. OUTPUT Error
+    """
 }
