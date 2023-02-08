8a509be80ac3ba67177cfb5ea0d9859dcada5467
MBX619 <123102601+MBX619@users.noreply.github.com>
2023-02-08 20:38:13 +0100

    task 10-11

--- src/main/kotlin/lms_126/Task10.kt
+++ src/main/kotlin/lms_126/Task10.kt
@@ -2,5 +2,14 @@ package lms_126
 
 fun task10(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        1.INPUT email
+        2.INPUT Password
+        3.IF email is mal-formed:
+                1.OUTPUT email error
+        4.ELSE
+              1. IF password is less than 6 chars long
+                    1. OUTPUT password error
+              2. ELSE create an account     
+    """
 }
--- src/main/kotlin/lms_126/Task11.kt
+++ src/main/kotlin/lms_126/Task11.kt
@@ -2,5 +2,13 @@ package lms_126
 
 fun task11(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        1.Separate unit from magnitude 
+        2.IF the unit is C
+               1.Transfer magnitude to f
+               2.OUTPUT results
+        3.ELSE the unit is F 
+             1. Transfer magnitude to C
+             2. OUTPUT result      
+    """
 }
